2f1c9266b0001b9ea08185e1b4e851237cd9cba5
aakulgina <51416227+aakulgina@users.noreply.github.com>
2019-12-29 15:46:57 +0400

    Updated spaces and deleted a var

--- jenkins_autostart.sh
+++ jenkins_autostart.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 docker pull jenkins
-export CONFIG_FOLDER=/opt/jenkins/config
-mkdir -p $CONFIG_FOLDER
-chown 744 $CONFIG_FOLDER
+mkdir -p /opt/jenkins/config
+chown 744 /opt/jenkins/config
 docker run -d --restart always -p 49001:8080 -p 8080:8080 -p 50000:50000 -v /opt/jenkins/config:/var/jenkins_home --name jenkins -t jenkins
 sudo systemctl disable jenkins.service
