1e79cb5b129ef4432dc8df3afe718c9c3518c01b
aakulgina <51416227+aakulgina@users.noreply.github.com>
2020-01-19 18:43:46 +0400

    Updated

--- jenkins_autostart.sh
+++ jenkins_autostart.sh
@@ -3,4 +3,4 @@ docker pull jenkins
 mkdir -p /opt/jenkins/config
 chown 744 /opt/jenkins/config
 docker run -d --restart always -p 49001:8080 -p 8080:8080 -p 50000:50000 -v /opt/jenkins/config:/var/jenkins_home --name jenkins -t jenkins
-sudo systemctl disable runjenkins.service
+# sudo systemctl disable runjenkins.service
